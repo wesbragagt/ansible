34b370f52910de80964c310a70eaf4aff38fd2eb
wesbragagt <weslley@bambee.com>
2022-08-09 11:37:04 -0500

    nix install works

--- scripts/1.nix.sh
+++ scripts/1.nix.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# This will perform a single-user installation of Nix, meaning that /nix is owned by the invoking user. You should run this under your usual user account, not as root. The script will invoke sudo to create /nix if it doesn’t already exist. If you don’t have sudo, you should manually create /nix first as root, e.g.:
-export NIX_INSTALLER_NO_MODIFY_PROFILE=true
-sh <(curl -L https://nixos.org/nix/install) --no-daemon
-export PATH=$HOME/.nix-profile/bin:$PATH
+sh <(curl -L https://nixos.org/nix/install) --daemon &&
+
+export PATH=/nix/var/nix/profiles/default/bin:$HOME/.nix-profile/bin:$PATH
+sudo chown -R $(whoami) /nix
